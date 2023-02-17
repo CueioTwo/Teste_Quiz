238933dc3d39a6955365a40b39686643bd4704cb
CueioTwo <112721682+CueioTwo@users.noreply.github.com>
2023-02-17 00:10:33 +0000

    My friend is DUMB

--- js/scrip.js
+++ js/scrip.js
@@ -1,4 +1,10 @@
 function Verificar(){
-    var txt1 = window.document.querySelector("input#res").value;
-    
+    var txt1 = window.document.querySelector("input#res");
+    var n1 = Number(txt1.value);
+    if(n1 == 19){
+      var verif = document.write("Correto")
+      verif.style.color = "green";
+    }else{
+      
+    }
 }
