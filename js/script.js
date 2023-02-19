af0ae0cf4b6ce5d14290e299684738c1403281f1
NicolasPIP <112721682+NicolasPIP@users.noreply.github.com>
2023-02-18 21:10:46 -0300

    0.1

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ function verif(){
     var res = window.document.getElementById('res')
     if(n1 == 21){
         res.innerHTML = "Resposta correta";
+        res.style.color = 'green';
     } else {
         res.innerHTML = "Resposta errada";
+        res.style.color = 'red';
     }
     }
